0d693e375b981d5dcacbeef5fd2402fa84d0075b
Paolo Davila <pdavila1313@gmail.com>
2016-01-25 16:44:26 +0100

    First version: Implemented STUPID CODE OK

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Invoices;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class InvoicesController
@@ -17,6 +19,35 @@ class InvoicesController extends Controller {
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index() {
-        return view('invoices');
+
+        if ( !Auth::check() ) {
+            return "Forbidden!";
+        }
+
+        $database_invoices = $this->getAllInvoicesFromDatabase();
+
+        $invoices = $this->transform($database_invoices);
+
+//      return view('invoices');
+        return view('invoices',compact('invoices'));
+
+//      $data['invoices'] = $invoices;
+//      return view('invoices',$data);
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getAllInvoicesFromDatabase() {
+        return Invoices::all();
+    }
+
+    /**
+     * @param $database_invoices
+     * @return mixed
+     */
+    private function transform($database_invoices) {
+        //Nothing here -> no transformations example
+        return $database_invoices;
     }
 }
--- resources/views/invoices.blade.php
+++ resources/views/invoices.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.app')
 
 @section('htmlheader_title')
-    Home
+    Invoices
 @endsection
-
 
 @section('main-content')
     <div class="container spark-screen">
@@ -11,7 +10,7 @@
             <div class="col-xs-12">
                 <div class="box">
                     <div class="box-header">
-                        <h3 class="box-title">Responsive Hover Table</h3>
+                        <h3 class="box-title">Invoices</h3>
 
                         <div class="box-tools">
                             <div class="input-group input-group-sm" style="width: 150px;">
@@ -22,17 +21,19 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <!-- /.box-header -->
+                    </div> <!-- /.box-header -->
+
+
                     <div class="box-body table-responsive no-padding">
                         <table class="table table-hover">
                             <tr>
-                                <th>ID</th>
-                                <th>User</th>
-                                <th>Date</th>
-                                <th>Status</th>
-                                <th>Reason</th>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>Created at</th>
+                                <th>Updated at</th>
+                                <th>Total</th>
                             </tr>
+                            
                             <tr>
                                 <td>183</td>
                                 <td>John Doe</td>
@@ -40,32 +41,9 @@
                                 <td><span class="label label-success">Approved</span></td>
                                 <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                             </tr>
-                            <tr>
-                                <td>219</td>
-                                <td>Alexander Pierce</td>
-                                <td>11-7-2014</td>
-                                <td><span class="label label-warning">Pending</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                            </tr>
-                            <tr>
-                                <td>657</td>
-                                <td>Bob Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="label label-primary">Approved</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                            </tr>
-                            <tr>
-                                <td>175</td>
-                                <td>Mike Doe</td>
-                                <td>11-7-2014</td>
-                                <td><span class="label label-danger">Denied</span></td>
-                                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                            </tr>
                         </table>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-                <!-- /.box -->
+                    </div> <!-- /.box-body -->
+                </div> <!-- /.box -->
             </div>
         </div>
     </div>
