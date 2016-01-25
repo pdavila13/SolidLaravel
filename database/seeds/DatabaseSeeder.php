b7a3deac38f99cf0ea9fe21d910ab5ae3064c1de
Paolo Davila <pdavila1313@gmail.com>
2016-01-25 16:18:16 +0100

    Add invoices migration and seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,29 @@
 <?php
 
+use App\Invoices;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         // $this->call(UserTableSeeder::class);
+
+        $faker = Faker\Factory::create();
+        $this->seedInvoices($faker);
+    }
+
+    private function seedInvoices($faker) {
+        foreach (range(0,100) as $number) {
+            $invoice = new Invoices();
+
+            $invoice->name = $faker->sentence;
+            $invoice->totalAmmount = $faker->randomNumber;
+            $invoice->save();
+        }
     }
 }
